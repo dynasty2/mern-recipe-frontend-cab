6755f5ccfa269c74e038dc4b30d95669b8f2cb26
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-18 16:11:40 -0500

    app initial links setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home';
 import Fridge from './components/Fridge/Fridge';
-import Recipe from './components/Recipe/Recipe';
+import Recipes from './components/Recipe/Recipes';
 import User from './components/User/User';
 
-function App() {
-  return (
-    <div>
-      <h1>Yo my guy. Waz good?</h1>
-    </div>
-  );
-}
+class App extends Component {
+  // constructor(props){
+  //   super(props);
 
+  //   this.state = { 
+
+  //   };
+  // }
+
+render() {
+  return (
+    <Router>
+    <div className="container">
+   
+              <Link to="/home" className="nav-link">Home</Link>
+           
+              <Link to="/fridge" className="nav-link">Your Fridge</Link>
+         
+              <Link to="/recipes" className="nav-link">Recipes</Link>
+
+              <Link to="/sign-in" className="nav-link">Sign In</Link>
+     
+    {/* <Route path="/" exact component={Home} />
+    <Route path="/fridge" component={Fridge} />
+    <Route path="/recipes" component={Recipes} /> 
+    <Route path="/user" component={User} /> */}
+    </div> 
+    </Router>
+  
+  )
+};
+  }
 export default App;
+
