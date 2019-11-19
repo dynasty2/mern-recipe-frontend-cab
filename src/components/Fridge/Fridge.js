2e9ba8f1a0bad9285e8e14e6f7298ccc2c495396
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-19 11:48:04 -0500

    adjusted fridge form and fridge

--- src/components/Fridge/Fridge.js
+++ src/components/Fridge/Fridge.js
@@ -7,21 +7,32 @@ export default class Fridge extends Component {
 
 render() {
         return (
-            <form>
-            <div className="form-group">
+            <div className="container">
+            <form style={{ marginTop: 20 }}>
+            {/* <div className="form-group">
               <label for="exampleInputEmail1">Type of Food</label>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Type"/>
-            </div>
+            </div> */}
             <div className="form-group">
               <label for="exampleInputPassword1">Name of Food</label>
-              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Enter Name"/>
+              <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter Name"/>
             </div>
             {/* <div className="form-group form-check">
               <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
               <label className="form-check-label" for="exampleCheck1">Check me out</label>
             </div> */}
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Add to Fridge</button>
           </form>
+          <table className="table" style={{ marginTop: 20 }}>
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Food Name</th>
+              <th scope="col">Edit</th>
+            </tr>
+          </thead>
+          </table>
+          </div>
         )
     }
 }
