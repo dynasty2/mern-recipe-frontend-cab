41c8227998884a665c1f140f0c1bf6ee7fc292f4
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-19 09:55:56 -0500

    home header text centered

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
 render() {
     return (
         <header className="container-fluid p-3 mb-2 bg-light text-dark">
-            <h1>This is the Home page. Sit back and relax.</h1>
+            <h1 className="text-center">Fridge to Table.</h1>
         </header>
         )
     }
