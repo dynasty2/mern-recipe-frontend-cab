97d164872ca9579a4392923a4fd99a397b5dcb0e
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-19 15:37:31 -0500

    added remove button in fridge

--- src/components/Fridge/Fridge.js
+++ src/components/Fridge/Fridge.js
@@ -7,14 +7,14 @@ export default class Fridge extends Component {
 render() {
     return (
         <div>
-            <form>
+            <form style={{ margin: 20 }}>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Food Name</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Food Name"/>
+                    <label for="food">Food Name</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Food Name"/>
                 </div> 
                     <button type="submit" class="btn btn-primary">Add to Fridge</button>
             </form>
-                <table class="table table-hover">
+                <table class="table table">
                     <thead>
                         <tr>
                             <th scope="col">First</th>
@@ -23,8 +23,10 @@ render() {
                     </thead>
                     <tbody>
                         <tr>
-                             <td>Mark</td>
-                             <td>Edit</td>
+                             <td>Apple</td>
+                             <td>
+                             <button type="submit" class="btn-sm btn-danger">Remove</button>
+                             </td>
                         </tr>
 
                     </tbody>
