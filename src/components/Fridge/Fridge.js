c870e3a7861a42df336e84cd4104638f86f47e10
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-19 11:21:51 -0500

    initial fridge form setup

--- src/components/Fridge/Fridge.js
+++ src/components/Fridge/Fridge.js
@@ -7,9 +7,21 @@ export default class Fridge extends Component {
 
 render() {
         return (
-            <header className="container-fluid p-3 mb-2 bg-light text-dark">
-                <h1>What's in your Fridge?</h1>
-            </header>
+            <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Type of Food</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Type"/>
+            </div>
+            <div className="form-group">
+              <label for="exampleInputPassword1">Name of Food</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Enter Name"/>
+            </div>
+            {/* <div className="form-group form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+              <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div> */}
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         )
     }
 }
