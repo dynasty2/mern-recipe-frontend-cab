f22fb459766ce7be1b4a231a7b895524ce624f38
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-21 14:12:18 -0500

    trying to fix app for merge from dev branch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
-import React from 'react';
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home';
 import Fridge from './components/Fridge/Fridge';
-import Recipe from './components/Recipe/Recipe';
+import Recipes from './components/Recipe/Recipes';
 import User from './components/User/User';
+import FridgeRecipeRender from './components/FridgeRecipeRender/FridgeRecipeRender';
 
+class App extends Component {
+  // constructor(props){
+  //   super(props);
 
+  //   this.state = { 
 
-function App() {
+  //   };
+  // }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <Router>
+    <div className="container" style={{ marginTop: 20}}>
+   
+    <ul className="nav justify-content-center">
+  <li className="nav-item">
+    <Link to="/home" className="nav-link active" href="#">Home</Link>
+  </li>
+  <li className="nav-item">
+    <Link to="/fridge" className="nav-link" href="#">Your Fridge</Link>
+  </li>
+  <li className="nav-item">
+    <Link to="/recipes" className="nav-link" href="#">Recipes</Link>
+  </li>
+  <li className="nav-item">
+    <Link to="/sign_in" className="nav-link" href="#">Sign In</Link>
+  </li>
+</ul>
+     
+    <Route path="/home" exact component={Home} />
+    <Route path="/fridge" render={() => {
+      return <Fridge />
+    }
+      } />
+    <Route path="/recipes" component={Recipes} /> 
+    <Route path="/sign_in" component={User} />
+    </div> 
+    </Router>
+  
+  )
+};
+  }
 export default App;
