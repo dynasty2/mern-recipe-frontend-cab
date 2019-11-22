7485a8ba576415a65b7329eb79ec1ab33eaba7e0
Conor Johnston <cnrswim@yahoo.com>
2019-11-22 10:17:51 -0500

    log in form

--- src/components/User/SignUp.js
+++ src/components/User/SignUp.js
@@ -5,20 +5,24 @@ import { Form, Button } from 'react-bootstrap';
 export default class SignUp extends Component {
     render() {
         return(
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+            <div>
+                <h1>Sign Up</h1>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         );
     }
 }
