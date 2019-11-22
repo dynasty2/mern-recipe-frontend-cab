565b69d24cdefec0848de508f7c6705199f21b6e
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-11-22 12:06:17 -0500

    font change and media query on homeTitle

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,10 +13,10 @@ render() {
             <div className="mask rgba-black-light align-items-center">
               <div className="container">
                 <div className="row">
-                  <div className="col-md-12 mb-4 white-text text-center">
+                  <div className="homeTitle col-md-12 mb-4 white-text text-center">
                     <h1 className="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Fridge to Table</strong></h1>
                     <hr className="hr-light my-4"/>
-                    <h5 className="text-uppercase text-white"><strong>Meal Planning</strong></h5>
+                    <h5 className="mealPlan text-uppercase text-white"><strong>Meal Planning</strong></h5>
                   </div>
                 </div>
               </div>
