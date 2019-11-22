8d30d0821830b0969b95e7ff10621f0560c9bf27
Ben Miller <benbmiller123@gmail.com>
2019-11-22 11:02:00 -0500

    pulled recipes and ingredients into state, added filter on fridge field using ingredients

--- src/App.js
+++ src/App.js
@@ -1,63 +1,180 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home/Home';
-import Fridge from './components/Fridge/Fridge';
-import Recipes from './components/Recipe/Recipes';
-import User from './components/User/User';
-import FridgeRecipeRender from './components/FridgeRecipeRender/FridgeRecipeRender';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home/Home";
+import Fridge from "./components/Fridge/Fridge";
+import Recipes from "./components/Recipe/Recipes";
+import User from "./components/User/User";
+import FridgeRecipeRender from "./components/FridgeRecipeRender/FridgeRecipeRender";
 
 class App extends Component {
-  // constructor(props){
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  //   this.state = { 
+    this.state = {
+      recipes: [],
+      ingredients: []
+    };
 
-
-  //   };
-  // }
-
-
-
-
-
-render() {
-  return (
-    <Router>
-    <div className="container" style={{ marginTop: 20}}>
-   
-    <ul className="nav justify-content-center">
-  <li className="nav-item">
-    <Link to="/home" className="nav-link active" href="#">Home</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/fridge" className="nav-link" href="#">Your Fridge</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/recipes" className="nav-link" href="#">Recipes</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/sign_in" className="nav-link" href="#">Sign In</Link>
-  </li>
-</ul>
-     
-    <Route path="/home" exact component={Home} />
-    <Route path="/fridge" render={() => {
-      return <Fridge />
-    }
-      } />
-    <Route path="/recipes" component={Recipes} /> 
-    <Route path="/sign_in" component={User} />
-    </div> 
-    </Router>
-  
-  )
-};
+    this.handleAddFridge = this.handleAddFridge.bind(this);
+    this.handleSignIn = this.handleSignIn.bind(this);
   }
+
+  async componentDidMount() {
+    let hasMore = true;
+    let recipeArray = [];
+    let ingredientArray = [];
+    let nextUrl = "";
+    while (hasMore === true) {
+      if (recipeArray.length === 0) {
+        await fetch("http://fridge-to-table-cab.herokuapp.com/recipe")
+          .then(res => res.json())
+          .then(res => {
+            // hasMore = res.has_more;
+            console.log(res);
+            res.data.forEach(dat => {
+              recipeArray.push(dat);
+            });
+            nextUrl = res.next_page;
+            if (res.has_more === false) {
+              hasMore = false;
+            }
+          });
+      } else {
+        await fetch(nextUrl)
+          .then(res => res.json())
+          .then(res => {
+            // hasMore = res.has_more;
+            console.log(res);
+            res.data.forEach(dat => {
+              recipeArray.push(dat);
+            });
+            nextUrl = res.next_page;
+            if (res.has_more === false) {
+              hasMore = false;
+            }
+          });
+      }
+    }
+    hasMore = true;
+    while (hasMore === true) {
+      if (ingredientArray.length === 0) {
+        await fetch("http://fridge-to-table-cab.herokuapp.com/ingredient")
+          .then(res => res.json())
+          .then(res => {
+            // hasMore = res.has_more;
+            console.log(res);
+            res.data.forEach(dat => {
+              ingredientArray.push(dat);
+            });
+            nextUrl = res.next_page;
+            if (res.has_more === false) {
+              hasMore = false;
+            }
+          });
+      } else {
+        await fetch(nextUrl)
+          .then(res => res.json())
+          .then(res => {
+            // hasMore = res.has_more;
+            console.log(res);
+            res.data.forEach(dat => {
+              ingredientArray.push(dat);
+            });
+            nextUrl = res.next_page;
+            if (res.has_more === false) {
+              hasMore = false;
+            }
+          });
+      }
+    }
+
+    this.setState({
+      recipes: recipeArray,
+      ingredients: ingredientArray
+    });
+  }
+
+  handleAddFridge() {} //handles setting fridge state when a fridge is added to the database
+
+  handleSignIn() {} //handles when a user is signed in and sets the fridge state to the users fridge
+
+  render() {
+    let recipeObs = this.state.recipes.map(recipe => {
+      return <p>{recipe.name}</p>;
+    });
+    let ingredientObs = this.state.ingredients.map(ingredient => {
+      return <p>{ingredient.name}</p>;
+    });
+    return (
+      <Router>
+        <div className="container" style={{ marginTop: 20 }}>
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link active" href="#">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/fridge" className="nav-link" href="#">
+                Your Fridge
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/recipes" className="nav-link" href="#">
+                Recipes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/sign_in" className="nav-link" href="#">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+
+          <Route
+            path="/home"
+            exact
+            render={() => {
+              return (
+                <div>
+                  <Home />
+                  <h1>Recipes</h1>
+                  {recipeObs}
+                  <h1>ingredients</h1>
+                  {ingredientObs}
+                </div>
+              );
+            }}
+          />
+          <Route
+            path="/fridge"
+            render={() => {
+              return (
+                <Fridge
+                  ingredients={this.state.ingredients}
+                  handleAddFridge={this.handleAddFridge}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/recipes"
+            render={() => {
+              return <Recipes recipes={this.state.recipes} />;
+            }}
+          />
+          <Route path="/sign_in" component={User} />
+        </div>
+      </Router>
+    );
+  }
+}
 export default App;
 
-{/* <ul className="nav justify-content-center">
+{
+  /* <ul className="nav justify-content-center">
   <li className="nav-item">
     <Link to="/home" className="nav-link active" href="#">Home</Link>
   </li>
@@ -70,4 +187,5 @@ export default App;
   <li className="nav-item">
     <Link to="/sign_in" className="nav-link" href="#">Sign In</Link>
   </li>
-</ul> */}
+</ul> */
+}
--- src/components/Fridge/Fridge.js
+++ src/components/Fridge/Fridge.js
@@ -1,94 +1,119 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import FormFridge from '../../components/FormFridge/FormFridge';
-import FridgeFood from '../../components/FridgeFood/FridgeFood';
-import FridgeRecipeRender from '../../components/FridgeRecipeRender/FridgeRecipeRender';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import FormFridge from "../../components/FormFridge/FormFridge";
+import FridgeFood from "../../components/FridgeFood/FridgeFood";
+import FridgeRecipeRender from "../../components/FridgeRecipeRender/FridgeRecipeRender";
 
-class Fridge extends Component { 
-//constructor: state set as an array: wordArray = ["apple", "banana"] and bind handleFormSubmit
-//method: handleFormSubmit(String) make a new variable and set it to the state version of wordArray (https://davidwalsh.name/javascript-clone-array) and .push the String. use this.setState({ wordArray: <new array with String pushed in> })
+class Fridge extends Component {
+  //constructor: state set as an array: wordArray = ["apple", "banana"] and bind handleFormSubmit
+  //method: handleFormSubmit(String) make a new variable and set it to the state version of wordArray (https://davidwalsh.name/javascript-clone-array) and .push the String. use this.setState({ wordArray: <new array with String pushed in> })
 
-constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-        food: "",
-        item: [],
-        ingrName: ["apple", "salt"]
-    }
+      food: "",
+      item: [],
+      ingrName: props.ingredients,
+      noMatchFood: ""
+    };
+
+    this.noMatch = false;
 
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.deleteFood = this.deleteFood.bind(this);
-};
-
-  handleFormSubmit = (e) => {
-      e.preventDefault();
-
-      let items = this.state.item.slice(0);
-
-      items.push(this.state.food);
-
-    this.setState({
-        item: items,
-        food: ""
-    })
   }
 
-deleteFood = (e) => {
+  checkForMatch() {
+    if (this.state.ingrName.includes(this.state.food)) {
+      return true;
+    }
+    return false;
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    let noMatchFood = "";
+    let items = this.state.item.slice(0);
+    if (this.checkForMatch()) {
+      items.push(this.state.food);
+      this.noMatch = false;
+    } else {
+      this.noMatch = true;
+      noMatchFood = this.state.food;
+    }
+
+    this.setState({
+      item: items,
+      food: "",
+      noMatchFood: noMatchFood
+    });
+  };
+
+  deleteFood = e => {
     e.preventDefault();
 
     let items = this.state.item.slice(0);
     let index = e.target.parentNode.parentNode.dataset.key;
     items[index] = items[items.length - 1];
+    let dont = false;
+    if (parseInt(index, 10) === items.length - 1) {
+      dont = true;
+    }
     items.pop();
     let temp = items[index];
     let past = false;
-    items.forEach((item, i) => {
-        if(parseInt(index, 10) === i){
-            past = true;
-            items[i] = items[i + 1];
-           
-        } else if(past && i !== (items.length - 1)) {
-            items[i] = items[i + 1];
+    if (!dont) {
+      items.forEach((item, i) => {
+        if (parseInt(index, 10) === i) {
+          past = true;
+          items[i] = items[i + 1];
+        } else if (past && i !== items.length - 1) {
+          items[i] = items[i + 1];
         } else if (i === items.length - 1) {
-            items[i] = temp;
+          items[i] = temp;
         } else {
-            items[i] = item;
+          items[i] = item;
         }
-    })
-    
+      });
+    }
 
     this.setState({
-        item: items
-    })
-}
+      item: items
+    });
+  };
 
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     let value = e.target.value;
     this.setState({
       food: value
-    })
-  }
+    });
+  };
 
-render() {
-
-    return (
-        <div>
-            <div className="float-left">
-            <FormFridge handleFormSubmit={ this.handleFormSubmit } 
-                        handleInputChange={ this.handleInputChange }
-                        food={ this.state.food } />
-            <FridgeFood items={ this.state.item } 
-                        deleteFood={this.deleteFood}/>
-            </div>
-            <div className="float-right" style={{ marginTop: 160 }}>
-            <FridgeRecipeRender />
-            </div>
-        </div>
-            )
-        }
+  render() {
+    let errorMes = <p></p>;
+    if (this.noMatch === true) {
+      errorMes = <p>Error: {this.state.noMatchFood} is not an ingredient</p>;
     }
+    return (
+      <div>
+        <div className="float-left">
+          <FormFridge
+            handleFormSubmit={this.handleFormSubmit}
+            handleInputChange={this.handleInputChange}
+            food={this.state.food}
+            errorMes={errorMes}
+          />
+          <FridgeFood items={this.state.item} deleteFood={this.deleteFood} />
+        </div>
+        <div className="float-right" style={{ marginTop: 160 }}>
+          <FridgeRecipeRender />
+        </div>
+      </div>
+    );
+  }
+}
 
-    export default Fridge;
+export default Fridge;
