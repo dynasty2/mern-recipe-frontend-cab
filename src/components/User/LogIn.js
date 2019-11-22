6c73c427ecbbc9c54ec25d9895834563cac3a6a2
Conor Johnston <cnrswim@yahoo.com>
2019-11-22 10:33:09 -0500

    formatting

--- src/components/User/LogIn.js
+++ src/components/User/LogIn.js
@@ -5,24 +5,24 @@ import { Form, Button } from 'react-bootstrap';
 export default class LogIn extends Component {
     render() {
         return(
-        <div>
-            <h1>Log In</h1>
+            <div>
+                <h1>Log In</h1>
 
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </div>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         );
     }
 }
