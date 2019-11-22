995ff2b912f76bdd56ab7dcd81c3a4ba16dc0f2c
Conor Johnston <cnrswim@yahoo.com>
2019-11-22 14:32:08 -0500

    signup fix

--- src/App.js
+++ src/App.js
@@ -1,142 +1,161 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home/Home';
-import Fridge from './components/Fridge/Fridge';
-import Recipes from './components/Recipe/Recipes';
-import User from './components/User/User';
-import SignUp from './components/User/SignUp';
-import LogIn from './components/User/LogIn';
-import LogOut from './components/User/LogOut';
-import FridgeRecipeRender from './components/FridgeRecipeRender/FridgeRecipeRender';
-import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home/Home";
+import Fridge from "./components/Fridge/Fridge";
+import Recipes from "./components/Recipe/Recipes";
+import User from "./components/User/User";
+import SignUp from "./components/User/SignUp";
+import LogIn from "./components/User/LogIn";
+import LogOut from "./components/User/LogOut";
+import FridgeRecipeRender from "./components/FridgeRecipeRender/FridgeRecipeRender";
+import axios from "axios";
 
 class App extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
 
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       isLoggedIn: false
-    }
+    };
 
-    this.handleLogOut = this.handleLogOut.bind(this)
-    this.handleInput = this.handleInput.bind(this)
-    this.handleLogIn = this.handleLogIn.bind(this)
-    this.handleSignUp = this.handleSignUp.bind(this)
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleLogIn = this.handleLogIn.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
   }
   componentDidMount() {
     if (localStorage.token) {
       this.setState({
-        isLoggedIn: true,
-      })
+        isLoggedIn: true
+      });
     } else {
       this.setState({
         isLoggedIn: false
-      })
+      });
     }
   }
-  
+
   handleLogOut() {
     this.setState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       isLoggedIn: false
-    })
-    localStorage.clear()
+    });
+    localStorage.clear();
   }
 
   handleInput(e) {
     this.setState({
       [e.target.name]: e.target.value
-  })
-
+    });
   }
 
   handleSignUp(e) {
-      e.preventDefault()
-      console.log(this.state)
-    axios.post('https://fridge-to-table-cab.herokuapp.com/users/signup', {
-      email: this.state.email,
-      password: this.state.password
-    })
-    .then(Response => {
-      localStorage.token = Response.data.token
-      this.setState({ isLoggedIn: true })
-    })
-    .catch(err => console.log(err))
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post("https://fridge-to-table-cab.herokuapp.com/users/signup", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(Response => {
+        localStorage.token = Response.data.token;
+        this.setState({ isLoggedIn: true });
+      })
+      .catch(err => console.log(err));
   }
 
   handleLogIn(e) {
-    e.preventDefault()
-    console.log("click")
-    axios.post('https://fridge-to-table-cab.herokuapp.com/users/login', {
-      email: this.state.email,
-      password: this.state.password
-    })
-    .then(response => {
-      localStorage.token = response.data.token
-      this.setState({isLoggedIn: true})
-      console.log(this.state.isLoggedIn)
-    })
-    .catch(err => console.log(err))
+    e.preventDefault();
+    console.log("click");
+    axios
+      .post("https://fridge-to-table-cab.herokuapp.com/users/login", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        localStorage.token = response.data.token;
+        this.setState({ isLoggedIn: true });
+        console.log(this.state.isLoggedIn);
+      })
+      .catch(err => console.log(err));
 
-    console.log(this.state.isLoggedIn)
+    console.log(this.state.isLoggedIn);
   }
 
-render() {
-  return (
-    <Router>
-    <div className="container" style={{ marginTop: 20}}>
-   
-    <ul className="nav justify-content-center">
-  <li className="nav-item">
-    <Link to="/home" className="nav-link active" href="#">Home</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/fridge" className="nav-link" href="#">Fridge</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/recipes" className="nav-link" href="#">Recipes</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/signup" className="nav-link" href="#">Sign Up</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/signin" className="nav-link" href="#">Log In</Link>
-  </li>
-  <li className="nav-item">
-    <Link to="/logout" className="nav-link" href="#">Log Out</Link>
-  </li>
-</ul>
+  render() {
+    return (
+      <Router>
+        <div className="container" style={{ marginTop: 20 }}>
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link active" href="#">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/fridge" className="nav-link" href="#">
+                Fridge
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/recipes" className="nav-link" href="#">
+                Recipes
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link" href="#">
+                Sign Up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signin" className="nav-link" href="#">
+                Log In
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/logout" className="nav-link" href="#">
+                Log Out
+              </Link>
+            </li>
+          </ul>
 
-    <Route path="/" exact component={Home} />
-    <Route path="/home" exact component={Home} />
-    <Route path="/fridge" render={() => {
-      return <Fridge />
-    }
-      } />
-    <Route path="/recipes" component={Recipes} /> 
-    <Route path="/signup" render={signUpProp => (
+          <Route path="/" exact component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route
+            path="/fridge"
+            render={() => {
+              return <Fridge />;
+            }}
+          />
+          <Route path="/recipes" component={Recipes} />
+          <Route
+            path="/signup"
+            render={signUpProp => (
               <SignUp
-                signUpProp = {this.handleInput}               
-              />)}
+                handleInput={this.handleInput}
+                handleSignUp={this.handleSignUp}
               />
-    <Route path="/signin" render={logInProp => (
-              <LogIn 
-              logInProp = {this.handleInput}
-              />
-            )} />
-    <Route path="/logout" render={logOutProp => (
-      <LogOut 
-      logOutProp = {this.handleLogOut}/>
-    )} />
-    </div> 
-    </Router>
-  
-  )
-};
+            )}
+          />
+          <Route
+            path="/signin"
+            render={logInProp => (<LogIn 
+              handleInput={this.handleInput} 
+              handleLogin={this.handleLogOut}
+            />)}
+          />
+          <Route
+            path="/logout"
+            render={logOutProp => <LogOut logOutProp={this.handleLogOut} />}
+          />
+        </div>
+      </Router>
+    );
   }
+}
 export default App;
--- src/components/User/SignUp.js
+++ src/components/User/SignUp.js
@@ -2,32 +2,27 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button } from 'react-bootstrap';
 
-export default class SignUp extends Component {
-    constructor(props) {
-        super(props);
-    }
+export default function SignUp(props) {
 
-    render() {
-        console.log(this.props);
-        return(
-            <div>
-                <h1>Sign Up</h1>
+    console.log(props);
+    return(
+        <div>
+            <h1>Sign Up</h1>
 
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control name="email" type="email" placeholder="Enter email" onChange={this.props.signUpProp}/>
-                    </Form.Group>
+            <Form onSubmit={props.handleSignUp}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name="email" type="email" placeholder="Enter email" onChange={props.handleInput}/>
+                </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control name="password" type="password" placeholder="Password" onChange={this.props.signUpProp}/>
-                    </Form.Group>
-                    <Button variant="primary" type="button" onClick={this.props.handleSignUp}>
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        );
-    }
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name="password" type="password" placeholder="Password" onChange={props.handleInput}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
 }
